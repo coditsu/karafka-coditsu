803f83f197d57c2e76ab81c313a12b5f8172494e
Maciej Mensfeld <maciej@mensfeld.pl>
2018-08-03 13:16:44 +0200

    remarks

--- lib/karafka_coditsu/response_builder.rb
+++ lib/karafka_coditsu/response_builder.rb
@@ -23,7 +23,7 @@ module KarafkaCoditsu
       # @param method_name [Symbol] name of method
       # @param include_private [Boolean] should be included in private checks
       # @return [Boolean] do we take care of this missing method
-      def respond_to_missing?(method_name, include_private = false)
+      def respond_to_missing?(_method_name, _include_private = false)
         true
       end
     end
